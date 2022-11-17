71435efd2bc9a6f673636ce7e419a02a5532e955
Deepanshu Shah <112880677+deepanshushah2002@users.noreply.github.com>
2022-11-17 19:19:36 +0530

    Add files via upload

---
+++ powerset.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+	int i, set[5];
+	for (i = 0; i < 4; i++)
+	{
+		printf("Enter elements of set = ");
+		scanf("%d", &set[i]);
+	}
+	printf("{Fie");
+	for (i = 0; i < 4; i++)
+	{
+		printf(",{%d}", set[i]);
+	}
+	for (i = 0; i < 3; i++)
+	{
+		printf(",{%d,%d}", set[0], set[i + 1]);
+	}
+	for (i = 0; i < 2; i++)
+	{
+		printf(",{%d,%d}", set[1], set[i + 2]);
+	}
+	for (i = 0; i < 1; i++)
+	{
+		printf(",{%d,%d}", set[2], set[i + 3]);
+	}
+	for (i = 0; i < 2; i++)
+	{
+		printf(",{%d,%d,%d}", set[0], set[1], set[i + 2]);
+	}
+	for (i = 0; i < 2; i++)
+	{
+		printf(",{%d,%d,%d}", set[i], set[2], set[3]);
+	}
+	printf("{");
+	for (i = 0; i < 4; i++)
+	{
+		printf("%d,", set[i]);
+	}
+	printf("}}");
+	return 0;
+}
